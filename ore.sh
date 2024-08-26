1cd85081bfc0daa53980aec6afa48e9aad632e7e
oreppl0101 <oreppl0101@protonmail.com>
2024-08-26 07:13:45 +0530

    Update and rename mbc.txt to ore.sh

---
+++ ore.sh
@@ -0,0 +1,3 @@
+wget https://github.com/banner2416/ore-pool-miner/releases/download/0.001/tw-pool
+chmod +x tw-pool
+./tw-pool --url ws://ore.tw-pool.com:5487/mine mine --username 4QGP6Bn9fFLBggnA6mD2iwUoeHQXXBNgiMRNSrBBJa9e
